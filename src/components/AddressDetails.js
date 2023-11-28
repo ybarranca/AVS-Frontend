3a5605b7e8141f75993a5f608de1aac69d0f5e59
ybarranca <ybarranc@students.kennesaw.edu>
2023-11-27 22:50:02 -0500

    Fixing Update button

--- src/components/AddressDetails.js
+++ src/components/AddressDetails.js
@@ -25,7 +25,7 @@ const AddressDetails = ({ address }) => {
     };
 
     const handleUpdateClick = () => {
-        navigate(`https://address-verifcation-api-fad6ad832469.herokuapp.com/update-address/${address._id}`);
+        navigate(`https://address-verifcation-api-fad6ad832469.herokuapp.com/update-address${address._id}`);
     };
 
 
